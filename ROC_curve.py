2066bad0859a8010dc9f3df7a419f033a3708730
Wenjian-Ma <35370756+Wenjian-Ma@users.noreply.github.com>
2024-02-26 23:42:48 +0800

    Create ROC_curve.py

---
+++ ROC_curve.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc,precision_recall_curve
+from matplotlib import rcParams
+from matplotlib.ticker import MaxNLocator
+config = {
+            #"font.family": 'serif',
+            "font.size": 23,
+           # "mathtext.fontset": 'stix',
+           #"font.sans-serif": ['SimHei'],#宋体
+            "font.family":'Times New Roman',
+           # 'axes.unicode_minus': False # 处理负号
+         }
+rcParams.update(config)
+#plt.rcParams['font.sans-serif'] = ['KaiTi']
+
+label_Test_60 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/label_Test_60.npy')
+score_Test_60 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/score_Test_60.npy')
+
+label_Test_315 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/label_Test_315.npy')
+score_Test_315 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/score_Test_315.npy')
+
+label_Btest_31 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/label_Btest_31.npy')
+score_Btest_31 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/score_Btest_31.npy')
+
+label_UBtest_31 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/label_UBtest_31.npy')
+score_UBtest_31 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/score_UBtest_31.npy')
+
+# label01 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/label01.npy')
+# score01 = np.load('/media/ST-18T/Ma/LGS-PPIS/curve/score01.npy')
+
+
+
+fpr_Test_60, tpr_Test_60, threshold_Test_60 = roc_curve(label_Test_60, score_Test_60)
+auc_Test_60 = auc(fpr_Test_60, tpr_Test_60)  # 计算AUC值
+
+# precision, recall, thresholds = precision_recall_curve(label_Test_60, score_Test_60)
+
+fpr_Test_315, tpr_Test_315, threshold_Test_315 = roc_curve(label_Test_315, score_Test_315)
+auc_Test_315 = auc(fpr_Test_315, tpr_Test_315)  # 计算AUC值
+
+# precision_Test_60, recall_Test_60, thresholds = precision_recall_curve(label_Test_60, score_Test_60)
+
+fpr_Btest_31, tpr_Btest_31, threshold_Btest_31 = roc_curve(label_Btest_31, score_Btest_31)
+auc_Btest_31 = auc(fpr_Btest_31, tpr_Btest_31)  # 计算AUC值
+
+# precision, recall, thresholds = precision_recall_curve(label_Test_60, score_Test_60)
+
+# fpr_Btest_31, tpr_Btest_31, threshold_Btest_31 = roc_curve(label_Btest_31, score_Btest_31)
+# auc_Btest_31 = auc(fpr_Btest_31, tpr_Btest_31)  # 计算AUC值
+
+fpr_UBtest_31, tpr_UBtest_31, threshold_UBtest_31 = roc_curve(label_UBtest_31, score_UBtest_31)
+auc_UBtest_31 = auc(fpr_UBtest_31, tpr_UBtest_31)  # 计算AUC值
+
+# precision, recall, thresholds = precision_recall_curve(label_Test_60, score_Test_60)
+
+line_width = 1.5 # 曲线的宽度
+plt.figure(figsize=(10, 7))  # 图的大小
+
+plt.plot(fpr_Test_60, tpr_Test_60, lw=line_width, label='Test_60 (AUROC = %0.3f)' % auc_Test_60, color='red')
+
+plt.plot(fpr_Test_315, tpr_Test_315, lw=line_width, label='Test_315 (AUROC = %0.3f)' % auc_Test_315, color='blue')
+
+plt.plot(fpr_Btest_31, tpr_Btest_31, lw=line_width, label='Btest_31 (AUROC = %0.3f)' % auc_Btest_31, color='green')
+
+plt.plot(fpr_UBtest_31, tpr_UBtest_31, lw=line_width, label='UBtest_31 (AUROC = %0.3f)' % auc_UBtest_31, color='orange')
+
+# plt.plot(fpr01, tpr01, lw=line_width, label='Multi-species-1%% (AUROC = %0.4f)' % auc01, color='grey')
+
+
+plt.xlim([0.0, 1.0])  # 限定x轴的范围
+plt.ylim([0.0, 1.0])  # 限定y轴的范围
+
+# y_ticks = [0.0,0.980,0.985,0.990,0.995,1.000]
+
+# plt.xticks(range(0, 10, 1)) # 修改x轴的刻度
+# plt.yticks(range(0, 10, 1)) # 修改y轴的刻度
+
+plt.xlabel('False Positive Rate')  # x坐标轴标题
+plt.ylabel('True Positive Rate')  # y坐标轴标题
+# plt.xlabel('Recall')
+# plt.ylabel('Precision')
+
+# plt.title('Receiver Operating Characteristic')  # 图标题
+
+plt.grid(linestyle='--')  # 在图中添加网格
+
+plt.legend(loc="lower right")  # 显示图例并指定图例位置
+
+
+# 5.中文处理问题
+# plt.rcParams['font.sans-serif'] = ['SimHei']
+# plt.rcParams['axes.unicode_minus'] = False
+
+
+# 6.展示图片和保存
+plt.savefig('/media/ST-18T/Ma/LGS-PPIS/curve/ROC.jpg', dpi=300)
